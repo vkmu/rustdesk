0096834b314e960e1543dc30af5f9e65a6989da6
qmkszht <mkszht@outlook.com>
2025-08-23 13:55:15 +0800

    remove option

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -2937,10 +2937,7 @@ Future<void> start_service(bool is_start) async {
 }
 
 Future<bool> canBeBlocked() async {
-  var access_mode = await bind.mainGetOption(key: kOptionAccessMode);
-  var option = option2bool(kOptionAllowRemoteConfigModification,
-      await bind.mainGetOption(key: kOptionAllowRemoteConfigModification));
-  return access_mode == 'view' || (access_mode.isEmpty && !option);
+  return false;
 }
 
 // to-do: web not implemented
