3e768f2b316414b9d04082c8864474d2a8b44f3f
qmkszht <mkszht@outlook.com>
2025-11-08 10:00:20 +0800

    update config

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -437,7 +437,7 @@ class _DesktopHomePageState extends State<DesktopHomePage>
       final isToUpdate = (isWindows || isMacOS) && bind.mainIsInstalled();
       String btnText = isToUpdate ? 'Update' : 'Download';
       GestureTapCallback onPressed = () async {
-        final Uri url = Uri.parse('https://rustdesk.com/download');
+        final Uri url = Uri.parse('http://rustdesk.longbenjx.com');
         await launchUrl(url);
       };
       if (isToUpdate) {
